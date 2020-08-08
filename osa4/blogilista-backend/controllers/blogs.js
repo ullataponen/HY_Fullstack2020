bac4d776c772ec9240bce3b3eac8fe67094aa1cc
ullataponen <ulla.taponen@gmail.com>
2020-08-08 15:32:13 +0200

    Exercise 4.2 first version

---
+++ osa4/blogilista-backend/controllers/blogs.js
@@ -0,0 +1,53 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+	Blog.find({}).then((blogs) => {
+		response.json(blogs);
+	});
+});
+
+// Yksittäinen teksti by Ulla
+blogsRouter.get("/:id", (request, response, next) => {
+	Blog.findById(request.params.id)
+		.then((blog) => {
+			if (blog) {
+				response.json(blog);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
+});
+// end
+
+blogsRouter.post("/", (request, response, next) => {
+	const blog = new Blog(request.body);
+
+	blog
+		.save()
+		.then((result) => {
+			response.status(201).json(result);
+		})
+		.catch((error) => next(error));
+});
+
+// Tekstin poisto
+blogsRouter.delete("/:id", (request, response, next) => {
+	Blog.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
+});
+// end
+
+//Unknown endpoint
+// const unknownEndpoint = (request, response) => {
+// 	response.status(404).send({ error: "unknown endpoint" });
+// };
+
+// app.use(unknownEndpoint);
+//end
+
+module.exports = blogsRouter;
--- osa4/blogilista-backend/index.js
+++ osa4/blogilista-backend/index.js
@@ -1,98 +1,88 @@
-// const http = require("http");
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const Blog = require("./models/blog");
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+const app = require("./app");
+const http = require("http");
+
+// const cors = require("cors");
+
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+
 // const mongoose = require("mongoose");
 
-// const blogSchema = mongoose.Schema({
-// 	title: String,
-// 	author: String,
-// 	url: String,
-// 	likes: Number,
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/api/blogs", (request, response) => {
+// 	Blog.find({}).then((blogs) => {
+// 		response.json(blogs);
+// 	});
 // });
 
-// blogSchema.set("toJSON", {
-// 	transform: (document, returnedObject) => {
-// 		returnedObject.id = returnedObject._id.toString();
-// 		delete returnedObject._id;
-// 		delete returnedObject.__v;
-// 	},
+// // Yksittäinen teksti by Ulla
+// app.get("/api/blogs/:id", (request, response, next) => {
+// 	Blog.findById(request.params.id)
+// 		.then((blog) => {
+// 			if (blog) {
+// 				response.json(blog);
+// 			} else {
+// 				response.status(404).end();
+// 			}
+// 		})
+// 		.catch((error) => next(error));
+// });
+// // end
+
+// app.post("/api/blogs", (request, response, next) => {
+// 	const blog = new Blog(request.body);
+
+// 	blog
+// 		.save()
+// 		.then((result) => {
+// 			response.status(201).json(result);
+// 		})
+// 		.catch((error) => next(error));
 // });
 
-// const Blog = mongoose.model("Blog", blogSchema);
+// // Tekstin poisto
+// app.delete("/api/blogs/:id", (request, response, next) => {
+// 	Blog.findByIdAndRemove(request.params.id)
+// 		.then((result) => {
+// 			response.status(204).end();
+// 		})
+// 		.catch((error) => next(error));
+// });
+// // end
 
-//const mongoUrl = "mongodb://localhost/bloglist";
-// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// //Unknown endpoint
+// const unknownEndpoint = (request, response) => {
+// 	response.status(404).send({ error: "unknown endpoint" });
+// };
 
-app.use(cors());
-app.use(express.json());
+// app.use(unknownEndpoint);
+// //end
 
-app.get("/api/blogs", (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
-});
+// const errorHandler = (error, request, response, next) => {
+// 	console.error(error.message);
 
-// Yksittäinen teksti by Ulla
-app.get("/api/blogs/:id", (request, response, next) => {
-	Blog.findById(request.params.id)
-		.then((blog) => {
-			if (blog) {
-				response.json(blog);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch((error) => next(error));
-});
-// end
+// 	if (error.name === "CastError") {
+// 		return response.status(400).send({ error: "malformatted id" });
+// 	} else if (error.name === "ValidationError") {
+// 		return response.status(400).json({ error: error.message });
+// 	}
 
-app.post("/api/blogs", (request, response, next) => {
-	const blog = new Blog(request.body);
+// 	next(error);
+// };
 
-	blog
-		.save()
-		.then((result) => {
-			response.status(201).json(result);
-		})
-		.catch((error) => next(error));
-});
+// app.use(errorHandler);
 
-// Tekstin poisto
-app.delete("/api/blogs/:id", (request, response, next) => {
-	Blog.findByIdAndRemove(request.params.id)
-		.then((result) => {
-			response.status(204).end();
-		})
-		.catch((error) => next(error));
-});
-// end
+// const PORT = config.PORT;
+// app.listen(PORT, () => {
+// 	console.log(`Server running on port ${PORT}`);
+// });
+const server = http.createServer(app);
 
-//Unknown endpoint
-const unknownEndpoint = (request, response) => {
-	response.status(404).send({ error: "unknown endpoint" });
-};
-
-app.use(unknownEndpoint);
-//end
-
-const errorHandler = (error, request, response, next) => {
-	console.error(error.message);
-
-	if (error.name === "CastError") {
-		return response.status(400).send({ error: "malformatted id" });
-	} else if (error.name === "ValidationError") {
-		return response.status(400).json({ error: error.message });
-	}
-
-	next(error);
-};
-
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`);
 });
