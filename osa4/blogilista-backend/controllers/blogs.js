a4ad4326f5d16405cdecaabc0868c4e7d93f9550
ullataponen <ulla.taponen@gmail.com>
2020-08-20 20:31:03 +0200

    Exercise 4.8, 4.10

--- osa4/blogilista-backend/controllers/blogs.js
+++ osa4/blogilista-backend/controllers/blogs.js
@@ -1,44 +1,50 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
+blogsRouter.get("/", async (request, response) => {
+	const blogs = await Blog.find({});
+	// response.json(blogs);
+	response.json(blogs.map((blog) => blog.toJSON()));
 });
 
 // Yksittäisen tekstin haku
-blogsRouter.get("/:id", (request, response, next) => {
-	Blog.findById(request.params.id)
-		.then((blog) => {
-			if (blog) {
-				response.json(blog);
-			} else {
-				response.status(404).end();
-			}
-		})
-		.catch((error) => next(error));
+blogsRouter.get("/:id", async (request, response) => {
+	const blog = await Blog.findById(request.params.id);
+
+	if (blog) {
+		response.json(blog);
+	} else {
+		response.status(404).end();
+	}
 });
 // end
 
-blogsRouter.post("/", (request, response, next) => {
-	const blog = new Blog(request.body);
+blogsRouter.post("/", async (request, response) => {
+	// const blog = new Blog(request.body);
 
-	blog
-		.save()
-		.then((result) => {
-			response.status(201).json(result);
-		})
-		.catch((error) => next(error));
+	// blog
+	// 	.save()
+	// 	.then((result) => {
+	// 		response.status(201).json(result);
+	// 	})
+	// 	.catch((error) => next(error));
+	const body = request.body;
+
+	const blog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: 0,
+	});
+
+	const savedBlog = await blog.save();
+	response.json(savedBlog.toJSON());
 });
 
 // Tekstin poisto
-blogsRouter.delete("/:id", (request, response, next) => {
-	Blog.findByIdAndRemove(request.params.id)
-		.then((result) => {
-			response.status(204).end();
-		})
-		.catch((error) => next(error));
+blogsRouter.delete("/:id", async (request, response) => {
+	await Blog.findByIdAndRemove(request.params.id);
+	response.status(204).end();
 });
 // end
 
--- osa4/blogilista-backend/tests/blog_api.test.js
+++ osa4/blogilista-backend/tests/blog_api.test.js
@@ -1,36 +1,26 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
-
-const initialBlogs = [
-	{
-		title: "Canonical string reduction",
-		author: "Edsger W. Dijkstra",
-		url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-		likes: 12,
-	},
-	{
-		title: "First class tests",
-		author: "Robert C. Martin",
-		url:
-			"http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-		likes: 12,
-	},
-];
+const { json } = require("express");
 
 beforeEach(async () => {
 	await Blog.deleteMany({});
 
-	let blogObject = new Blog(initialBlogs[0]);
-	await blogObject.save();
+	const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+	const promiseArray = blogObjects.map((blog) => blog.save());
+	await Promise.all(promiseArray);
 
-	blogObject = new Blog(initialBlogs[1]);
-	await blogObject.save();
+	// let blogObject = new Blog(helper.initialBlogs[0]);
+	// await blogObject.save();
+
+	// blogObject = new Blog(helper.initialBlogs[1]);
+	// await blogObject.save();
 });
 
-test.only("blogs are returned as json", async () => {
+test("blogs are returned as json", async () => {
 	await api
 		.get("/api/blogs")
 		.expect(200)
@@ -40,7 +30,7 @@ test.only("blogs are returned as json", async () => {
 test("all blogs are returned", async () => {
 	const response = await api.get("/api/blogs");
 
-	expect(response.body).toHaveLength(initialBlogs.length);
+	expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
 test("a specific blog is within the returned blogs", async () => {
@@ -49,6 +39,65 @@ test("a specific blog is within the returned blogs", async () => {
 	const title = response.body.map((r) => r.title);
 
 	expect(title).toContain("Canonical string reduction");
+});
+
+test("a valid blog can be added", async () => {
+	const newBlog = {
+		title: "Saving that last penny",
+		author: "Roope Ankka",
+		url: "www.rankka.com",
+		likes: 123,
+	};
+
+	await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
+
+	const blogsAtEnd = await helper.blogsInDb();
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+	const title = blogsAtEnd.map((b) => b.title);
+	expect(title).toContain("Saving that last penny");
+});
+
+test("blog without title, author and url is not added", async () => {
+	const newBlog = {
+		likes: 666,
+	};
+	await api.post("/api/blogs").send(newBlog).expect(400);
+
+	const blogsAtEnd = await helper.blogsInDb();
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+});
+
+test("a specific blog can be viewed", async () => {
+	const blogsAtStart = await helper.blogsInDb();
+
+	const blogToView = blogsAtStart[0];
+
+	const resultBlog = await api
+		.get(`/api/blogs/${blogToView.id}`)
+		.expect(200)
+		.expect("Content-Type", /application\/json/);
+
+	const processedBlogToView = JSON.parse(JSON.stringify(blogToView));
+
+	expect(resultBlog.body).toEqual(processedBlogToView);
+});
+
+test("a blog can be deleted", async () => {
+	const blogsAtStart = await helper.blogsInDb();
+	const blogToDelete = blogsAtStart[0];
+
+	await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+	const blogsAtEnd = await helper.blogsInDb();
+	expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+
+	const title = blogsAtEnd.map((b) => b.title);
+	expect(title).not.toContain(blogToDelete.title);
 });
 
 afterAll(() => {
