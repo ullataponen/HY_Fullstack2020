7ea019002c93b4b4cd4db0690e5e1746354a6fbc
Ulla <ulla.taponen@gmail.com>
2020-10-24 17:49:33 +0300

    Exercise 5.1

---
+++ osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import Notification from "./components/Notification";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+	const [blogs, setBlogs] = useState([]);
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [username, setUsername] = useState([]);
+	const [password, setPassword] = useState([]);
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		blogService.getAll().then((blogs) => setBlogs(blogs));
+	}, []);
+
+	const handleLogin = async (event) => {
+		event.preventDefault();
+
+		try {
+			const user = await loginService.login({
+				username,
+				password,
+			});
+			setUser(user);
+			setUsername("");
+			setPassword("");
+		} catch (exception) {
+			setErrorMessage("Wrong username or password");
+			setTimeout(() => {
+				setErrorMessage(null);
+			}, 5000);
+		}
+	};
+
+	const loginForm = () => (
+		<>
+			<h2>Log in to application</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username
+					<input
+						type="text"
+						value={username}
+						name="Username"
+						onChange={({ target }) => setUsername(target.value)}
+					/>
+				</div>
+				<div>
+					password
+					<input
+						type="password"
+						value={password}
+						name="Password"
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</div>
+				<button type="submit">login</button>
+			</form>
+		</>
+	);
+
+	const blogList = () => (
+		<>
+			<h2>blogs</h2>
+			{user.name} logged in
+			{blogs.map((blog) => (
+				<Blog key={blog.id} blog={blog} />
+			))}
+		</>
+	);
+
+	return (
+		<div>
+			<Notification message={errorMessage} />
+			{user === null ? loginForm() : blogList()}
+		</div>
+	);
+};
+
+export default App;
