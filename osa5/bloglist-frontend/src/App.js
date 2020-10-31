a84e0de93243a31f75514e7fb174f05ab76b37a5
Ulla <ulla.taponen@gmail.com>
2020-10-31 12:04:44 +0200

    Exercise 5.3

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
+import BlogForm from "./components/BlogForm";
 import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
@@ -10,6 +11,11 @@ const App = () => {
 	const [username, setUsername] = useState([]);
 	const [password, setPassword] = useState([]);
 	const [user, setUser] = useState(null);
+	// const [newBlog, setNewBlog] = useState({
+	// 	title: "",
+	// 	author: "",
+	// 	url: "",
+	// });
 
 	useEffect(() => {
 		blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -33,6 +39,7 @@ const App = () => {
 				password,
 			});
 			window.localStorage.setItem("loggedInUser", JSON.stringify(user));
+			blogService.setToken(user.token);
 			setUser(user);
 			setUsername("");
 			setPassword("");
@@ -50,8 +57,22 @@ const App = () => {
 		blogService.setToken(null);
 	};
 
+	// const handleBlogChange = (event) => {
+	// 	console.log(event.target.value);
+	// 	setNewBlog({ ...newBlog, [event.target.name]: event.target.value });
+	// };
+
+	const addBlog = (newBlog) => {
+		// event.preventDefault();
+
+		blogService.create(newBlog).then((returnedBlog) => {
+			setBlogs(blogs.concat(returnedBlog));
+			// setNewBlog({ title: "", author: "", url: "" });
+		});
+	};
+
 	const loginForm = () => (
-		<>
+		<div>
 			<h2>Log in to application</h2>
 			<form onSubmit={handleLogin}>
 				<div>
@@ -74,24 +95,54 @@ const App = () => {
 				</div>
 				<button type="submit">login</button>
 			</form>
-		</>
+		</div>
 	);
 
 	const blogList = () => (
-		<>
-			<h2>blogs</h2>
-			{user.name} logged in
-			<button onClick={handleLogout}>Log out</button>
+		<div>
+			<h2>Blogs</h2>
+
 			{blogs.map((blog) => (
 				<Blog key={blog.id} blog={blog} />
 			))}
-		</>
+		</div>
 	);
+
+	// const blogForm = () => (
+	// 	<div>
+	// 		<h2>Create new</h2>
+	// 		<form onSubmit={addBlog}>
+	// 			<label htmlFor="title">Title</label>
+	// 			<input name="title" value={newBlog.title} onChange={handleBlogChange} />
+	// 			<br />
+	// 			<label htmlFor="author">Author</label>
+	// 			<input
+	// 				name="author"
+	// 				value={newBlog.author}
+	// 				onChange={handleBlogChange}
+	// 			/>
+	// 			<br />
+	// 			<label htmlFor="url">URL</label>
+	// 			<input name="url" value={newBlog.url} onChange={handleBlogChange} />
+	// 			<br />
+	// 			<button type="submit">Submit</button>
+	// 		</form>
+	// 	</div>
+	// );
 
 	return (
 		<div>
 			<Notification message={errorMessage} />
-			{user === null ? loginForm() : blogList()}
+			{user === null ? (
+				loginForm()
+			) : (
+				<div>
+					<p>{user.name} logged in</p>
+					<button onClick={handleLogout}>Log out</button>
+					<BlogForm addBlog={addBlog} />
+					{blogList()}
+				</div>
+			)}
 		</div>
 	);
 };
