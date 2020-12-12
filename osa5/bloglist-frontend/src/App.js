9a8d869ce93431632b3580d24ce735627198f88f
ullataponen <ulla.taponen@gmail.com>
2020-12-12 14:09:00 +0200

    Excercise 5.9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -118,9 +118,11 @@ const App = () => {
     <div>
       <h2>Blogs</h2>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
+      {blogs
+        .sort((curr, prev) => prev.likes - curr.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
     </div>
   );
 
